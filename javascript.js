c1ef4b80473347470f088f9d2822857abd49b6f8
seongho00 <tjdwns000927@gmail.com>
2025-09-21 21:27:52 +0900

    사진 안 넘겨지는 버그 고침

--- javascript.js
+++ javascript.js
@@ -1,25 +1,28 @@
-$(function(){
-    $(".photo").hover(function(){
+$(function () {
+    $(".blog-card").hover(function () {
         let $slides = $(this).find(".slide");
         let index = 0;
 
-        // 첫 번째 이미지만 보이게
-        $slides.hide().eq(index).show().addClass("active");
 
-        // ✅ interval 설정
+        // 첫 번째 이미지만 보이게
+        $slides.hide();
+        $($slides[0]).show();
+
+        // interval 설정
         let interval = setInterval(() => {
-            $slides.eq(index).removeClass("active").fadeOut(500);
+
+            $($slides[index]).fadeOut(500);
             index = (index + 1) % $slides.length;
-            $slides.eq(index).addClass("active").fadeIn(500);
+            $($slides[index]).fadeIn(500);
         }, 1500);
 
-        // interval 저장
         $(this).data("interval", interval);
 
-    }, function(){
-        // 마우스 벗어나면 멈추고 첫 번째 이미지 복귀
+    }, function () {
+
         clearInterval($(this).data("interval"));
         let $slides = $(this).find(".slide");
-        $slides.hide().removeClass("active").first().show().addClass("active");
+        $slides.hide();
+        $($slides[0]).show();
     });
 });
